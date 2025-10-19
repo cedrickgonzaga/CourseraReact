bf885c5e3aa2215bb5da4bb03a44c37c4511b3b6
Cedrick Gonzaga <cedrickgonzaga016@gmail.com>
2025-10-20 00:05:41 +0800

    Fix router for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";    
-import { store } from "./redux/store.js";   
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter basename="/">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
